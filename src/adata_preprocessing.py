0e5b13b189c03898fc72243b5f48c3e65d4d61f7
Bluedragon2513 <Bluedragon2513@gmail.com>
2024-04-15 17:12:33 -0700

    Transfer to PC

--- src/adata_preprocessing.py
+++ src/adata_preprocessing.py
@@ -39,6 +39,19 @@ def filter_celltypes(adata):
         adatas.append(new_adata)
     return adatas
 
+def filter_hvg(adata):
+    hvg_filter = sc.pp.highly_variable_genes(adata, layer='counts', n_top_genes=2000, n_bins=20, flavor="cell_ranger", inplace=False)['highly_variable']
+    anndata = ad.AnnData(
+        X=adata.X[:,hvg_filter],
+        obs=adata.obs.copy(),
+    )
+    anndata.layers['counts'] = adata.layers['counts'][:,hvg_filter]
+    return anndata
+
+def scib_normalize(adata):
+    sc.pp.normalize_total(adata, target_sum=1e6) # each cell has the same total count after normalization
+    sc.pp.log1p(adata) # so that every value is applied log(1+X)
+
 # main method
 if __name__ == "__main__":
     adata = sc.read_h5ad("data/preprocessed/human_pancreas_norm_complexBatch.h5ad")
