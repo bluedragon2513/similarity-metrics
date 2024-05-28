628c1f4dc8254249bc80d2faf25bf9d9aac6f224
Bluedragon2513 <Bluedragon2513@gmail.com>
2024-05-27 21:13:28 -0700

    Merging

--- src/metrics/Seurat/Seurat.R
+++ src/metrics/Seurat/Seurat.R
@@ -1,18 +1,41 @@
 library(Seurat)
+library(glue)
+library(SeuratObject)
 library(future)
 library(pbapply)
 library(future.apply)
-library(glue)
-source("Seurat/seurat_code/utilities.R")
-source("Seurat/seurat_code/clustering.R")
-source("Seurat/seurat_code/convenience.R")
-source("Seurat/seurat_code/data.R")
-source("Seurat/seurat_code/dimensional_reduction.R")
-source("Seurat/seurat_code/mixscape.R")
-source("Seurat/seurat_code/objects.R")
-source("Seurat/seurat_code/RcppExports.R")
-source("Seurat/seurat_code/reexports.R")
+options(repos = c(CRAN = "https://cran.r-project.org"))
 
+my_list <- list(
+    "Seurat", "Rcpp", "dplyr", "ggplot2", "cowplot",
+    "Matrix", "reticulate", "RANN", "RColorBrewer", "igraph", "irlba",
+    "leiden", "lmtest", "MASS", "matrixStats", "pbapply", "plotly",
+    "png", "progressr", "purrr", "RcppAnnoy", "RcppHNSW", "RSpectra",
+    "Rtsne", "scattermore", "sctransform", "shiny", "spatstat.explore",
+    "spatstat.geom", "tibble", "uwot", "fitdistrplus", "future",
+    "future.apply", "generics", "ggrepel", "ggridges", "httr", "ica",
+    "jsonlite", "lifecycle", "miniUI", "patchwork", "ROCR",
+    "rlang", "tools", "utils", "stats", "ape", "rsvd", "testthat",
+    "hdf5r", "S4Vectors", "SummarizedExperiment", "SingleCellExperiment",
+    "BiocGenerics", "GenomicRanges", "GenomeInfoDb", 
+    "IRanges", "Rfast2", "Biobase", "VGAM",
+    "enrichR", "mixtools", "data.table",
+    "R.utils", "DelayedArray", "harmony"
+)
+
+for (pkg in my_list) {
+    library(pkg, character.only = TRUE)
+}
+source("Seurat/seurat_code/R/utilities.R")
+source("Seurat/seurat_code/R/clustering.R")
+source("Seurat/seurat_code/R/convenience.R")
+source("Seurat/seurat_code/R/data.R")
+source("Seurat/seurat_code/R/dimensional_reduction.R")
+source("Seurat/seurat_code/R/mixscape.R")
+source("Seurat/seurat_code/R/objects.R")
+source("Seurat/seurat_code/R/RcppExports.R")
+source("Seurat/seurat_code/R/reexports.R")
+source("Seurat/seurat_code/R/integration.R")
 
 preprocessing <- function(list) {
     for (i in 1:length(list)) {
@@ -31,13 +54,22 @@ run_seurat <- function(path) {
         seurat_object <- readRDS(paste0(directory, "/", file_names[i]))
         data.list <- c(data.list, list(seurat_object))
     }
-    anchors <- FindIntegrationAnchors(object.list = data.list, anchor.features = 30)
-    source("Seurat/seurat_code/integration.R")
-    matrix <- IntegrateData(anchors)
-    data_frame <- as.data.frame(matrix)
-    for(i in 1:length(file_names)) {
-        names(data_frame)[names(data_frame) == glue("V{i}")] <- glue("{file_names[i]}")
-        rownames(data_frame)[i] <- glue("{file_names[i]}")
-    }
-    write.csv(data_frame, "/Users/carsonnannini/Research/similarity-metrics/data/pancreas/celltype_scores_seurat.csv")
+    anchors <- FindIntegrationAnchors(data.list,
+                                      l2.norm = FALSE,
+                                      reduction = "cca",
+                                      normalization.method = NULL,
+                                      dims = 1:50,
+                                      k.anchor = 20,
+                                      k.filter = 800,
+                                      k.score = 20)
+    cells_1 <- ncol(data.list[[1]])
+    cells_2 <- ncol(data.list[[2]])
+    score <- max(anchors[[1]][[1]] / cells_1, anchors[[1]][[2]] / cells_2)
+    # matrix <- IntegrateData(anchors, normalization.method = NULL)
+    # data_frame <- as.data.frame(matrix)
+    # for(i in 1:length(file_names)) {
+    #     names(data_frame)[names(data_frame) == glue("V{i}")] <- glue("{file_names[i]}")
+    #     rownames(data_frame)[i] <- glue("{file_names[i]}")
+    # }
+    write.csv(score, "/Users/carsonnannini/Research/similarity-metrics/data/pancreas/celltype_scores_seurat.csv")
 }
--- src/metrics/Seurat/seurat.py
+++ src/metrics/Seurat/seurat.py
@@ -3,6 +3,50 @@ import anndata as ad
 import pickle
 import scanpy as sc
 import rpy2.robjects as robjects
+import importlib.util
+import sys
+sys.path.append("src/library/adata_preprocessing.py")
+from library.adata_preprocessing import scib_normalize
+import pandas as pd
+import numpy as np
+
+
+def seurat(datasets, normalize=False, **kwargs):
+    if normalize:
+        adata = ad.AnnData(np.concatenate((datasets[0], datasets[1])))
+        adata = scib_normalize(adata)
+        datasets[0] = ad.AnnData(adata.X[:len(datasets[0])])
+        datasets[1] = ad.AnnData(adata.X[len(datasets[0]):])
+        datasets[0].layers['counts'] = datasets[0].X
+        datasets[1].layers['counts'] = datasets[1].X
+    else:
+        adata1 = ad.AnnData(datasets[0])
+        adata1.layers['counts'] = adata1.X
+        datasets[0] = adata1
+        adata2 = ad.AnnData(datasets[1])
+        adata2.layers['counts'] = adata2.X
+        datasets[1] = adata2
+
+    print(datasets[0].n_obs, datasets[1].n_obs)
+    if(datasets[0].n_obs <= 50):
+        datasets[0] = addCells(datasets[0])
+    if(datasets[1].n_obs <= 50):
+        datasets[1] = addCells(datasets[1])
+    print(datasets[0].n_obs, datasets[1].n_obs)
+    adata_to_seurat(datasets[0], "dataset_1")
+    adata_to_seurat(datasets[1], "dataset_2")
+    run_seurat("Seurat/data/temp")
+    score = pd.read_csv('data/pancreas/celltype_scores_seurat.csv')
+    print(score.iloc[0].iloc[1])
+    return score.iloc[0].iloc[1]
+
+
+def addCells(dataset):
+    double_cells = dataset.n_obs + 65
+    num_features = dataset.n_vars
+    new_dataset = ad.AnnData(np.concatenate((dataset.X, np.random.randn(double_cells - dataset.n_obs, num_features))))
+    new_dataset.layers['counts'] = new_dataset.X
+    return new_dataset
 
 
 def read_file(path):
@@ -10,30 +54,32 @@ def read_file(path):
     return adata
 
 
-def rds_filter_batches(adata, filter_by):
+def rds_filter_batches(adata):
     adatas = {}
-    for c in adata.obs[filter_by].cat.categories:
+    for c in adata.obs['tech'].cat.categories:
         new_adata = ad.AnnData(
-            X=adata.layers['counts'][adata.obs[filter_by] == c, :],
-            obs=adata.obs[adata.obs[filter_by] == c]
+            X=adata.layers['counts'][adata.obs['tech'] == c, :],
+            obs=adata.obs[adata.obs['tech'] == c]
         )
         new_adata.layers['counts'] = new_adata.X
         adatas[c] = new_adata
     return adatas
 
-def adata_to_seurat(adata, filter_by):
-    adatas = rds_filter_batches(adata, filter_by)
-    for k,adata in adatas.items():
-        preprocessing.save_seurat(
-            adata = adata,
-            path = f"Seurat/data/{filter_by}/{k}.rds",
-            batch=k,
-        )
+def adata_to_seurat(adatas, name):
+    preprocessing.save_seurat(
+        adata = adatas,
+        path = f"Seurat/data/temp/{name}.rds",
+        batch="sample_id",
+    )
 
 def run_seurat(path):
-    robjects.r("source('src/Seurat.R')")
+    robjects.r("source('src/metrics/Seurat/Seurat.R')")
     function = robjects.r('run_seurat')
     r_string = robjects.StrVector([path])
     function(r_string)
 
 
+if __name__ == "__main__":
+    adata = read_file("data/unpreprocessed/human_pancreas_norm_complexBatch.h5ad")
+    dict = rds_filter_batches(adata)
+    retval = seurat([dict['inDrop1'], dict['inDrop1']], normalize=False)
