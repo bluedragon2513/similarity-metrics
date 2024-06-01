f5f4336a7e995c11eb9995a8c5f8b00c2345de74
carsonnannini <carsonnannini@arizona.edu>
2024-05-31 22:40:26 -0700

    updated dataframe in XML file

--- src/get_seurat.py
+++ src/get_seurat.py
@@ -1,17 +1,41 @@
 from scib import preprocessing
-import anndata
-import rpy2.robjects as ro
-import scanpy
+import anndata as ad
 import pickle
 import scanpy as sc
+import rpy2.robjects as robjects
 
 
-adata = sc.read_h5ad("scib/sim1_1_norm.h5ad")
+def rds_filter_batches(adata):
+    adatas = {}
+    for c in adata.obs['tech'].cat.categories:
+        new_adata = ad.AnnData(
+            X=adata.layers['counts'][adata.obs['tech'] == c, :],
+            obs=adata.obs[adata.obs['tech'] == c]
+        )
+        new_adata.layers['counts'] = new_adata.X
+        adatas[c] = new_adata
+    return adatas
 
-preprocessing.save_seurat(
-    adata=adata,
-    path="seurat_object.rds",
-    batch="sample_id",
-    hvgs=["batchname", "batchname_all", "final_call_label"] 
-)
 
+# def filter_batches(adata):
+#     adatas = []
+#     for c in adata.obs['tech'].cat.categories:
+#         new_adata = ad.AnnData(
+#             X=adata.layers['counts'][adata.obs['tech'] == c, :],
+#             obs=adata.obs[adata.obs['tech'] == c]
+#         )
+#         new_adata.layers['counts'] = new_adata.X
+#         adatas.append(new_adata)
+#     return adatas
+
+
+adata = sc.read_h5ad("/Users/carsonnannini/Research/similarity-metrics/data/preprocessed/human_pancreas_preprocessed.h5ad")
+
+
+adatas = rds_filter_batches(adata)
+for k,adata in adatas.items():
+    preprocessing.save_seurat(
+        adata = adata,
+        path = f"Seurat/data/{k}.rds",
+        batch=k,
+    )
